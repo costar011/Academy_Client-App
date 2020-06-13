d08ca2683acfd0796d3cf352232f0d2fb0288c34
YERIM <dnaer456@gmail.com>
2020-06-13 15:32:54 +0900

    Academy CSS add

--- src/screens/ACSC00.js
+++ src/screens/ACSC00.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import MainComponent1 from "../components/MainComponent1";
+import MainComponent2 from "../components/MainComponent2";
 
 class ACSC00 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentTab: 1,
+    };
+  }
   render() {
+    const { currentTab } = this.state;
     return (
       <div className="ACSC00">
         <div className="ACSC00__bg">
@@ -16,8 +26,41 @@ class ACSC00 extends React.Component {
             <ArrowDownwardIcon />
           </div>
         </div>
+        <div className="ACSC00_contents">
+          <button
+            className={
+              currentTab === 1
+                ? "ACSC00_contents_btn active"
+                : "ACSC00_contents_btn"
+            }
+            onClick={() => this._changeTabHandler(1)}
+          >
+            News
+          </button>
+          <button
+            className={
+              currentTab === 2
+                ? "ACSC00_contents_btn active"
+                : "ACSC00_contents_btn"
+            }
+            onClick={() => this._changeTabHandler(2)}
+          >
+            GALLERY
+          </button>
+        </div>
+        <div className="ACSC00_desc">
+          {currentTab === 1 ? <MainComponent1 /> : null}
+          {currentTab === 2 ? <MainComponent2 /> : null}
+        </div>
       </div>
     );
   }
+
+  _changeTabHandler = (tab) => {
+    this.setState({
+      currentTab: tab,
+    });
+  };
 }
+
 export default ACSC00;

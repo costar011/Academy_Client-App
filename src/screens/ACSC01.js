eeb2e05019ac854f46cd4929cf53a79356186542
YERIM <dnaer456@gmail.com>
2020-06-27 18:22:23 +0900

    add slick

--- src/screens/ACSC01.js
+++ src/screens/ACSC01.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Lnb from "../components/Lnb";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 class ACSC01 extends React.Component {
   constructor(props) {
@@ -22,6 +25,18 @@ class ACSC01 extends React.Component {
   render() {
     const { dataList } = this.state;
 
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      afterChange: () => console.log("afterChange"),
+      centerMode: true,
+    };
+
     return (
       <div className="ACSC01">
         <Lnb title="ABOUT" column="HOME > ABOUT" />
@@ -37,8 +52,38 @@ class ACSC01 extends React.Component {
             );
           })
         )}
+
+        <Slider {...settings}>
+          <div className="slick__box">
+            <h3 className="slick__box__1">1</h3>
+          </div>
+          <div className="slick__box">
+            <h3 className="slick__box__2">2</h3>
+          </div>
+          <div className="slick__box">
+            <h3 className="slick__box__3">3</h3>
+          </div>
+          <div className="slick__box">
+            <h3 className="slick__box__4">4</h3>
+          </div>
+          <div className="slick__box">
+            <h3 className="slick__box__5">5</h3>
+          </div>
+          <div className="slick__box">
+            <h3 className="slick__box__6">6</h3>
+          </div>
+        </Slider>
+
+        <div className="controller"></div>
+        <div className="chatArea"></div>
+        <input type="text" />
+        <input type="button" value="OK" onClick={this._inputHandler} />
       </div>
     );
   }
+
+  _inputHandler = () => {
+    alert("CLick");
+  };
 }
 export default ACSC01;

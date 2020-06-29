2f81092b38fc5d9f5c5859f692ddfbab084175ab
YERIM <dnaer456@gmail.com>
2020-06-29 21:20:00 +0900

    On

--- src/screens/ACSC03.js
+++ src/screens/ACSC03.js
@@ -1,11 +1,48 @@
 import React from "react";
 import Lnb from "../components/Lnb";
+import Fade from "react-reveal/Fade";
+import Flip from "react-reveal/Flip";
+import Bounce from "react-reveal/Bounce";
+import LightSpeed from "react-reveal/LightSpeed";
+import Slide from "react-reveal/Slide";
+import Zoom from "react-reveal/Zoom";
 
 class ACSC03 extends React.Component {
   render() {
     return (
       <div>
         <Lnb title="NOTICE" column="HOME > NOTICE" />
+        <Fade left>
+          <p>안녕하세요? 금학해법 수학학원 입니다.</p>
+        </Fade>
+
+        <Fade bottom>
+          <p>안녕하세요? 금학해법 수학학원 입니다.</p>
+        </Fade>
+
+        <Flip top>
+          <p>천둥번개</p>
+        </Flip>
+
+        <Bounce top>
+          <p>통통</p>
+        </Bounce>
+
+        <LightSpeed left>
+          <p>speed</p>
+        </LightSpeed>
+
+        <Slide bottom>
+          <p>ㅇㅅㅇ</p>
+        </Slide>
+
+        <Fade clear>
+          <p>clear</p>
+        </Fade>
+
+        <Zoom top>
+          <p>Zoom</p>
+        </Zoom>
       </div>
     );
   }
--- src/screens/ACSC04.js
+++ src/screens/ACSC04.js
@@ -2,12 +2,43 @@ import React from "react";
 import Lnb from "../components/Lnb";
 
 class ACSC04 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      txtValue: "",
+    };
+  }
   render() {
+    const { txtvalue } = this.state;
     return (
       <div>
         <Lnb title="CONTACT" column="HOME > CONTACT" />
+
+        <input
+          type="text"
+          id="tet"
+          name="txtvalue"
+          value={txtvalue}
+          onChange={this._BtnHandler}
+        ></input>
+        <input type="button" value="확인" onClick={this._OkHandler}></input>
       </div>
     );
   }
+
+  _BtnHandler = (event) => {
+    let nstate = {};
+
+    nstate[event.target.name] = event.target.txtvalue;
+
+    this.setState(nstate);
+  };
+
+  _OkHandler = () => {
+    const tet = document.getElementById("tet");
+
+    console.log(tet.value);
+  };
 }
 export default ACSC04;
